19c409c317ac04f3b2f1e10494f905bd69591fd1
David <David@172.32.40.165>
2023-08-25 10:36:55 -0500

    Ejercicio Resuelto Numero 4 - Entrega Numero 1

--- EjercicioResuelto4/src/main/java/com/mycompany/ejercicioresuelto4/EjercicioResuelto4.java
+++ EjercicioResuelto4/src/main/java/com/mycompany/ejercicioresuelto4/EjercicioResuelto4.java
@@ -4,13 +4,21 @@
 
 package com.mycompany.ejercicioresuelto4;
 
-/**
- *
- * @author David
- */
+import java.util.Scanner;
+
 public class EjercicioResuelto4 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int edalbert, edana, edmama, edjuan; //edad de juan
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese la edad de Juan");
+        edjuan = entrada.nextInt();//Edad de Juan ingresada por el usuario
+        edalbert = 2* (edjuan/3); //Edad de alberto 
+        edana = 4* (edjuan/3); //Edad de ana 
+        edmama = edalbert + edjuan + edana;//edad de madre 
+    System.out.println("Juan tiene :" + edjuan);
+    System.out.println("Aleberto tiene :" + edalbert);
+    System.out.println("Ana tiene :" + edana);
+    System.out.println("La madre tiene :" + edmama);
     }
 }
