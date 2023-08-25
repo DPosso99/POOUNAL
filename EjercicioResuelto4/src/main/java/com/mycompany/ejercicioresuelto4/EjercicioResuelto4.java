0f684eb8d40b901b5b27c35ca5514558b1b84832
DPosso99 <138802935+DPosso99@users.noreply.github.com>
2023-08-25 11:14:03 -0500

    EjercicioResuelto4.java

--- EjercicioResuelto4/src/main/java/com/mycompany/ejercicioresuelto4/EjercicioResuelto4.java
+++ EjercicioResuelto4/src/main/java/com/mycompany/ejercicioresuelto4/EjercicioResuelto4.java
@@ -9,14 +9,14 @@ import java.util.Scanner;
 public class EjercicioResuelto4 {
 
     public static void main(String[] args) {
-        int edalbert, edana, edmama, edjuan; //edad de juan
+        int edalbert, edana, edmama, edjuan; 
         Scanner entrada = new Scanner(System.in);
         System.out.println("Ingrese la edad de Juan");
         edjuan = entrada.nextInt();//Edad de Juan ingresada por el usuario
-        edalbert = 2* (edjuan/3); //Edad de alberto 
-        edana = 4* (edjuan/3); //Edad de ana 
-        edmama = edalbert + edjuan + edana;//edad de madre 
-    System.out.println("Juan tiene :" + edjuan);
+        edalbert = 2* (edjuan/3); //Edad de Alberto 
+        edana = 4* (edjuan/3); //Edad de Ana 
+        edmama = edalbert + edjuan + edana;//Edad de la madre 
+    System.out.println("Juan tiene :" + edjuan); 
     System.out.println("Aleberto tiene :" + edalbert);
     System.out.println("Ana tiene :" + edana);
     System.out.println("La madre tiene :" + edmama);
