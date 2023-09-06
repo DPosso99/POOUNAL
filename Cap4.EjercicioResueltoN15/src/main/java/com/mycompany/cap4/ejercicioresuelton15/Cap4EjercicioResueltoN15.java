1b88f56f2371c73477a30a60b74c6eee152a40ef
DPosso99 <138802935+DPosso99@users.noreply.github.com>
2023-09-05 23:12:20 -0500

    Cap4EjercicioResueltoN15.java

--- Cap4.EjercicioResueltoN15/src/main/java/com/mycompany/cap4/ejercicioresuelton15/Cap4EjercicioResueltoN15.java
+++ Cap4.EjercicioResueltoN15/src/main/java/com/mycompany/cap4/ejercicioresuelton15/Cap4EjercicioResueltoN15.java
@@ -35,44 +35,44 @@ public class Cap4EjercicioResueltoN15 {
       System.out.println("La esfera D es la diferente ");
     } else {
       if ((pesoA == pesoB) && (pesoA == pesoD)) { 
-        /*Aqui comparamos el peso de la esfera A con la B y D. 
+        /**Aqui comparamos el peso de la esfera A con la B y D. 
           Si son iguales, la esfera C seria la diferente.*/ 
         System.out.println("La esfera C es la diferente ");
             if (pesoD > pesoA){
                 System.out.println("la esfera D es la de mayor peso"); 
-                /*para saber si es de mayor o 
-                menor peso que las otras, basta compararla con el peso de cualquiera de las otras tres*/
+                /**para saber si es de mayor o menor peso que las otras, 
+                basta compararla con el peso de cualquiera de las otras tres*/
             }
             else {
                 System.out.println("La esfera D es la de menor peso ");
             } 
       } else if ((pesoA == pesoB) && (pesoA == pesoD)) { 
-        /*Aqui comparamos el peso de la esfera A con la B y D. 
+        /**Aqui comparamos el peso de la esfera A con la B y D. 
         Si son iguales, la esfera C seria la diferente.*/
         System.out.println("La esfera C es la diferente ");
             if (pesoC > pesoA){
                     System.out.println("la esfera C es la de mayor peso");
                     }
-                    /*para saber si es de mayor o menor peso que las otras,
+                    /**para saber si es de mayor o menor peso que las otras,
                      basta compararla con el peso de cualquiera de las otras tres*/
                     else {
                     System.out.println("La esfera C es la de menor peso ");
                     }
       } else if ((pesoA == pesoC) && (pesoA == pesoD)) { 
-        /*Aqui comparamos el peso de la esfera A con la C y D. 
+        /**Aqui comparamos el peso de la esfera A con la C y D. 
         Si son iguales, la esfera B seria la diferente.*/
         System.out.println("La esfera B es la diferente ");
              if (pesoB > pesoD){
                     System.out.println("la esfera B es la de mayor peso");
                     }
-                     /*para saber si es de mayor o menor peso que las otras,
+                     /**para saber si es de mayor o menor peso que las otras,
                       basta compararla con el peso de cualquiera de las otras tres*/
                     else {
                     System.out.println("La esfera B es la de menor peso ");
                     }
       } else { 
         System.out.println("La esfera A es la diferente " ); 
-        /*Aqui usamos el condicional else y entraria en esta instacia
+        /**Aqui usamos el condicional else y entraria en esta instancia
         si las anteriores condiciones no se cumplen por ende la esfera A seria la diferente.*/
             if (pesoA > pesoB){
                     System.out.println("la esfera A es la de mayor peso");
