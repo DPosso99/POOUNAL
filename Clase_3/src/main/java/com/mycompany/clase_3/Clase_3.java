c5447353d625aae4690e753653ebcc5ec3c67cbe
David <David@172.32.40.165>
2023-09-12 23:50:40 -0500

    Clase3

---
+++ Clase_3/src/main/java/com/mycompany/clase_3/Clase_3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clase_3;
+
+/**
+ *
+ * @author David
+ */
+public class Clase_3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
